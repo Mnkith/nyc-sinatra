70d7482827cd1e1d43d56a3b2dc4a827b799ac45
Mnkith <munkithabid@gmail.com>
2021-01-28 01:23:08 -0600

    commit tree b6d6752aa3f736c240e027d08bd3f9527abc6ef1

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,40 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do 
+    fig = Figure.create(params[:figure])
+    fig.titles << Title.create(params[:title]) if !params[:title][:name].empty?
+    fig.landmarks << Landmark.create(params[:landmark]) if !params[:landmark][:name].empty?
+    fig.save
+    redirect to "/figures/#{fig.id}"
+  end
+
+  get '/figures/:id' do
+    @fig = Figure.find params[:id]
+    erb :'/figures/show'
+  end
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find params[:id]
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do 
+    fig = Figure.find(params[:id])
+    fig.update(params[:figure])
+    fig.titles << Title.create(params[:title]) if !params[:title][:name].empty?
+    fig.landmarks << Landmark.create(params[:landmark]) if !params[:landmark][:name].empty?
+    fig.save
+    redirect to "/figures/#{fig.id}"
+  end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,32 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do 
+    lm = Landmark.create(params[:landmark])
+    lm.save
+    redirect to "/landmarks/#{lm.id}"
+  end
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find params[:id]
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find params[:id]
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    Landmark.find(params[:id]).update params[:landmark]
+    redirect to "/landmarks/#{params[:id]}"
+  end
+
+
 end
